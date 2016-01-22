e21ec2b072c6439e5fd4af4f018277c61bc945bc
PeterD23 <peterdavidson.23.94@gmail.com>
2016-01-22 15:32:14 +0000

    Initial Commit

---
+++ Source/Honours/Honours.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Honours.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Honours, "Honours" );
